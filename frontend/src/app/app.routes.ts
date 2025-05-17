4cff18abf84e4f2a5fdb5f390de7ec5f0d61afd9
Az Ale <azale2972@gmail.com>
2025-05-17 23:49:12 +0200

    EEE

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { FlightSearchComponent } from './home/flight-search.component';
-import { LoginSignupComponent } from './auth/login-signup.component';
+import { LoginSignupComponent } from './authentic/login-signup/login-signup.component';
 
 
 export const routes: Routes = [
