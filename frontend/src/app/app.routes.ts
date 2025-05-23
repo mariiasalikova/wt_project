da5597611acc51e4462108e24b2903271152e5de
Az Ale <azale2972@gmail.com>
2025-05-23 15:27:36 +0200

    eee

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { FlightSearchComponent } from './home/flight-search.component';
+import { FlightSearchComponent } from './search/flight-search.component';
 import { LoginSignupComponent } from './authentic/login-signup/login-signup.component';
 
 
